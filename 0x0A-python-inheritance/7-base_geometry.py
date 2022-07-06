829bf22ba32fd678ccf116665bde0d84f2d416b0
Olwanishola <lukusope@yahoo.com>
2022-07-06 10:38:19 -0700

    0x0A-python-inheritance/7-base_geometry.py

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,34 +1,19 @@
 #!/usr/bin/python3
 """
-This module implements a base class for Geometry objects,
-with attribute validator functions
-Raises:
-    Exception: area is not implemented
-    TypeError: [attribute] must be an integer
-    ValueError: [attribute] ,must be greater than 0
+class module
 """
 
 
 class BaseGeometry:
-    """base class
-    """
+    """Geometry class"""
+
     def area(self):
-        """find area
-        Raises:
-            Exception: area not implemented
-        """
-        raise Exception("area() is not implemented")
+        """raise exception is area is not implemented"""
+        raise Exception('area() is not implemented')
 
     def integer_validator(self, name, value):
-        """check value
-        Args:
-            name (str): value designation
-            value (any): value to check
-        Raises:
-            TypeError: value must be an integer
-            ValueError: value must be greater than 0
-        """
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
+        """check if value is an integer"""
+        if type(value) != int:
+            raise TypeError('{} must be an integer'.format(name))
         if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+            raise ValueError('{} must be greater than 0'.format(name))
